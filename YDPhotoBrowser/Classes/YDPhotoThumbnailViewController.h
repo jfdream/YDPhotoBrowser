969512a22b77cecdc25ba2baf18ce3b0e35a4e87
yangyudong <yangyudong@rongcloud.cn>
2018-07-22 23:17:59 +0800

    去掉相关log

---
+++ YDPhotoBrowser/Classes/YDPhotoThumbnailViewController.h
@@ -0,0 +1,12 @@
+//
+//  YDPhotoThumbnailViewController.h
+//  YDPhotoBrowser
+//
+//  Created by 杨雨东 on 2018/7/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YDPhotoThumbnailViewController : UIViewController
+
+@end
